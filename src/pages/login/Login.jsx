1b7db1b4192528cf95d87d95be3c63d16164e6f6
summer <levantung14112002@gmail.com>
2023-03-28 23:34:09 +0700

    Frontend Tung push code

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,51 @@
+import './login.scss'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { useState } from 'react';
+
+export default function Login() {
+    const [eye, setEye]= useState(false)
+    return (
+        <div className="login">
+            <div className="login-container">
+                <div className="header">
+                    <div className="heading">
+                        Đăng nhập
+                    </div>
+                    <p className="title">
+                        Bạn có thể đăng nhập bằng địa chỉ email của mình
+                    </p>
+                </div>
+                <form action="" className="content">
+                    <div className="input-group">
+                        <label htmlFor="email" className="label">Địa chỉ Email</label>
+                        <div className="input">
+                            <input type="text" name="email" id="email" />
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password" className="label">Mật khẩu</label>
+                        <div className="input">
+                            <input type={eye ? "text" : "password"} name="password" id="password" />
+                            {
+                                eye ? 
+                                <span onClick={() => {setEye(false)}}>
+                                    <VisibilityOffIcon />
+                                </span> :
+                                <span onClick={() => {setEye(true)}}>
+                                    <VisibilityIcon />
+                                </span>
+                            }
+                            
+                        </div>
+                    </div>
+                    
+                    <button>Đăng nhập</button>
+                    <div className="outer-link">
+                        Bạn không có tài khoản? Đăng ký ở đây
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,56 @@
+import './register.scss'
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { useState } from 'react';
+
+export default function Register() {
+    const [eye, setEye]= useState(false)
+    return (
+        <div className="register">
+            <div className="register-container">
+                <div className="header">
+                    <div className="heading">
+                        Đăng ký
+                    </div>
+                    <p className="title">
+                        Quá trình đăng ký chỉ mất chưa đầy một phút nhưng mang lại cho bạn toàn quyền kiểm soát các đơn đặt hàng của mình.
+                    </p>
+                </div>
+                <form action="" className="content">
+                    <div className="input-group">
+                        <label htmlFor="email" className="label">Họ và tên</label>
+                        <div className="input">
+                            <input type="text" name="email" id="email" />
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email" className="label">Địa chỉ Email</label>
+                        <div className="input">
+                            <input type="text" name="email" id="email" />
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password" className="label">Mật khẩu</label>
+                        <div className="input">
+                            <input type={eye ? "text" : "password"} name="password" id="password" />
+                            {
+                                eye ? 
+                                <span onClick={() => {setEye(false)}}>
+                                    <VisibilityOffIcon />
+                                </span> :
+                                <span onClick={() => {setEye(true)}}>
+                                    <VisibilityIcon />
+                                </span>
+                            }
+                            
+                        </div>
+                    </div>
+                    <button>Đăng ký</button>
+                    <div className="outer-link">
+                        Bạn đã có tài khoản? Đăng nhập
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
